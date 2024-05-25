79d504c388c57b799f8d4d469d1b902e6845c5c6
id-jyoti <157147388+id-jyoti@users.noreply.github.com>
2024-05-25 21:38:34 +0530

    changess

---
+++ custom.js
@@ -0,0 +1,1 @@
+$('.code-head').click(function () { $('.code-input').slideToggle(400); $(this).toggleClass('show'); }); $('.tab_menu ul li').click(function () { var index = $(this).index(); $(this).addClass('active'); $(this).siblings().removeClass('active'); $('.tab_details').eq(index).show(); $('.tab_details').eq(index).siblings().hide(); return false; });
